b56e8e08612c0d03b1387e062b578bb08092245a
Aziz-Rabya <azizrabya81@gmail.com>
2026-01-06 18:43:42 +0100

    implement auth, appwtite, and sentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,33 +1,36 @@
-import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox'
-import Rightsidebar from '@/components/Rightsidebar'
+import RightSidebar from '@/components/Rightsidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = () => {
-  const loggedIN = { firstName: 'Aziz', lastName: 'Rabya', email:'azizrabya@1gmail.com'}
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
+
   return (
-    <section className='home'>
-      <div className='home-content'>
-        <header className='home-header'>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
           <HeaderBox 
             type="greeting"
             title="Welcome"
-            user={loggedIN.firstName || 'Guest'}
+            user={loggedIn?.name || 'Guest'}
             subtext="Access and manage your account and transactions efficiently."
           />
+
           <TotalBalanceBox 
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.35}
           />
         </header>
-        RECENT TRANSACTIONS 
+
+        RECENT TRANSACTIONS
       </div>
 
-      <Rightsidebar 
-      user={loggedIN}
-      transactions={[]}
-      banks={[{ currentBalance: 1250.14 }, { currentBalance: 2250.14}]}
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50}]}
       />
     </section>
   )
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cc4731edd4df71a9d845eb9b23ce9c38@o4510664614543360.ingest.de.sentry.io/4510664618803280",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
